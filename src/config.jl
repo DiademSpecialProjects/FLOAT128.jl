7723a24db50963da8a5b61b3364bea7ad0bc994d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-02-23 06:46:45 -0500

    typealias--> const

--- src/config.jl
+++ src/config.jl
@@ -3,4 +3,4 @@
    If you want multiple Floating Point types available, use a Union here.
    For most purposes, it should be either Float64 or, e.g. for CUDA Float32.
 =#
-typealias FloatingPoint Float64
+const FloatingPoint = Float64
